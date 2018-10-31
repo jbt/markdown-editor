124f494abcbc7dc6c69d416a563efafab5531f05
Rohitt Vashishtha <aero31aero@gmail.com>
2018-10-31 09:10:44 +0530

    window: Make unsaved work warning less intrusive.

--- index.js
+++ index.js
@@ -357,6 +357,9 @@ function start() {
 }
 
 window.addEventListener("beforeunload", function (e) {
+    if (!editor.getValue() || editor.getValue() == localStorage.getItem('content')) {
+        return;
+    }
     var confirmationMessage = 'It looks like you have been editing something. '
                             + 'If you leave before saving, your changes will be lost.';
     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
