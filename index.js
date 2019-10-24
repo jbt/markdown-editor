40bc5251def08b99c0a4949a343d1dbbfb419c62
austinmm <austinmarino@Austins-MacBook-Pro.local>
2019-10-23 19:08:17 -0700

    Fixed bug with nested task-list items

--- index.js
+++ index.js
@@ -59,15 +59,15 @@ User: @austinmm
 */
 var render_tasklist = function(str){
     // Checked task-list box match
-	if(str.match(/<li>\[x\]\s+\w+<\/li>/gi)){
-        str = str.replace(/(<li)(>\[x\]\s+)(\w+<\/li>)/gi, 
+	if(str.match(/<li>\[x\]\s+\w+/gi)){
+        str = str.replace(/(<li)(>\[x\]\s+)(\w+)/gi, 
           `$1 style="list-style-type: none;"><input type="checkbox" 
           checked style="list-style-type: none; 
           margin: 0 0.2em 0 -1.3em;" disabled> $3`);
     }
     // Unchecked task-list box match
-    if (str.match(/<li>\[ \]\s+\w+<\/li>/gi)){
-        str = str.replace(/(<li)(>\[ \]\s+)(\w+<\/li>)/gi, 
+    if (str.match(/<li>\[ \]\s+\w+/gi)){
+        str = str.replace(/(<li)(>\[ \]\s+)(\w+)/gi, 
           `$1 style="list-style-type: none;"><input type="checkbox" 
             style="list-style-type: none; 
             margin: 0 0.2em 0 -1.3em;" disabled> $3`);
