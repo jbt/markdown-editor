a092a01f678b849269b44f2c162c088c0154b77a
= <cole7405bennett@gmail.com>
2019-10-27 15:54:20 -0700

    Convert plantuml text to generated images

--- index.js
+++ index.js
@@ -32,49 +32,49 @@ var hashto;
 
 /* Encode as Base 64 */
 function encode64(data) {
-	let r = "";
-	for (i = 0; i < data.length; i += 3) {
- 		if (i + 2 == data.length) {
-			r += append3bytes(data.charCodeAt(i), data.charCodeAt(i + 1), 0);
-		} else if (i + 1 == data.length) {
-			r += append3bytes(data.charCodeAt(i), 0, 0);
-		} else {
-			r += append3bytes(data.charCodeAt(i), data.charCodeAt(i + 1),
-				data.charCodeAt(i + 2));
-		}
-	}
-	return r;
+    let r = "";
+    for (i = 0; i < data.length; i += 3) {
+        if (i + 2 == data.length) {
+            r += append3bytes(data.charCodeAt(i), data.charCodeAt(i + 1), 0);
+        } else if (i + 1 == data.length) {
+            r += append3bytes(data.charCodeAt(i), 0, 0);
+        } else {
+            r += append3bytes(data.charCodeAt(i), data.charCodeAt(i + 1),
+                data.charCodeAt(i + 2));
+        }
+    }
+    return r;
 }
 
 function append3bytes(b1, b2, b3) {
-	c1 = b1 >> 2;
-	c2 = ((b1 & 0x3) << 4) | (b2 >> 4);
-	c3 = ((b2 & 0xF) << 2) | (b3 >> 6);
-	c4 = b3 & 0x3F;
-	r = "";
-	r += encode6bit(c1 & 0x3F);
-	r += encode6bit(c2 & 0x3F);
-	r += encode6bit(c3 & 0x3F);
-	r += encode6bit(c4 & 0x3F);
-	return r;
+    let c1 = b1 >> 2;
+    let c2 = ((b1 & 0x3) << 4) | (b2 >> 4);
+    let c3 = ((b2 & 0xF) << 2) | (b3 >> 6);
+    let c4 = b3 & 0x3F;
+    let r = "";
+    r += encode6bit(c1 & 0x3F);
+    r += encode6bit(c2 & 0x3F);
+    r += encode6bit(c3 & 0x3F);
+    r += encode6bit(c4 & 0x3F);
+    return r;
 }
 
 function encode6bit(b) {
-	if (b < 10) {
- 		return String.fromCharCode(48 + b);
-	}
-	b -= 10;
-	if (b < 26) {
- 		return String.fromCharCode(65 + b);
-	}
-	b -= 26;
-	if (b < 26) {
- 		return String.fromCharCode(97 + b);
-	}
-	b -= 26;
-	if (b == 0) return '-';
-	if (b == 1) return '_';
-	return '?';
+    if (b < 10) {
+        return String.fromCharCode(48 + b);
+    }
+    b -= 10;
+    if (b < 26) {
+        return String.fromCharCode(65 + b);
+    }
+    b -= 26;
+    if (b < 26) {
+        return String.fromCharCode(97 + b);
+    }
+    b -= 26;
+    if (b == 0) return '-';
+    if (b == 1) return '_';
+    return '?';
 }
 
 function generatePlantUML(s) {
@@ -108,6 +108,11 @@ function update(e) {
 function setOutput(val) {
     val = val.replace(/<equation>((.*?\n)*?.*?)<\/equation>/ig, function(a, b) {
         return '<img src="http://latex.codecogs.com/png.latex?' + encodeURIComponent(b) + '" />';
+    });
+
+    // Create PlantUML diagrams.
+    val = val.replace(/(?:```plantuml[\s|\n]+)([^`]*)(?:[\s|\n]*```)/ig, function(a, b) {
+        return generatePlantUML(b);
     });
 
     var out = document.getElementById('out');
